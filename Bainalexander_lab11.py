61d7082d21c592c9ae72bc3c4b5d1e393cc12558
abain42 <alex.bain208@gmail.com>
2024-11-29 22:40:51 -0500

    finished function

--- Bainalexander_lab11.py
+++ Bainalexander_lab11.py
@@ -5,10 +5,13 @@ from matplotlib import pyplot as plt
 
 sys.path.append("C:\\Users\\Alex\\Documents\\GitHub\\BainAlexander_Lab10")
 
-from BainAlexander_lab10 import make_tridiagonal, make_initialcond
 
-a = make_tridiagonal(5,3,1,4)
-print(a)
+from BainAlexander_lab10 import make_tridiagonal
+from BainAlexander_lab10 import make_initialcond
+from BainAlexander_lab10 import spectral_radius
+
+
+
 
 def advection1d(method, nspace, ntime, tau_rel,params):
     L, c = params
@@ -18,6 +21,8 @@ def advection1d(method, nspace, ntime, tau_rel,params):
     tau = tau_rel * tau_crit 
     t = np.arange(ntime) * tau 
 
+    a = np.zeros(nspace,ntime)
+    a[:, 0] = make_initialcond()
     for n in range(0,ntime-1):
         if method == "FTCS":
             A = make_tridiagonal(nspace, 1 - tau/h, 1, -1 + tau/h)
@@ -25,3 +30,10 @@ def advection1d(method, nspace, ntime, tau_rel,params):
             A = make_tridiagonal(nspace, 0.5 - tau/(2*h), 0, 0.5 + tau/(2*h))
         else: 
             return "INVALID METHOD"
+    
+    if spectral_radius(A) > 1:
+        print("Warning: Unstable integration requested!")
+
+    for n in range(1,ntime):
+        a[:, n+1] = np.dot(A, a[:, n])
+    return a,x,t
